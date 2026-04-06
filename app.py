1b8c239514581573ae9fd6e648cabeefce989375
adga33 <adga33@gmail.com>
2026-04-06 17:32:46 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -132,25 +132,19 @@ st.subheader("➕ Dodaj novi zapis")
 col1, col2 = st.columns(2)
 
 with col1:
+    colA, colB = st.columns([1, 1])
+
+with colA:
     datum = st.date_input("Datum")
-    sati = st.number_input("Trenutni radni sati", min_value=0, step=1)
+    sati = st.number_input("Radni sati", min_value=0, step=1)
+
+with colB:
     vrsta = st.selectbox(
-    "Vrsta unosa",
-    [
-        "Servis",
-        "Tehnički pregled",
-        "Popravak",
-        "Havarija",
-        "Remont",
-        "Izlaz",
-        "Ostalo"
-    ]
-)
-
-
-
-with col2:
+        "Vrsta unosa",
+        ["Servis", "Tehnički pregled", "Popravak", "Havarija", "Remont", "Izlaz", "Ostalo"]
+    )
     napomena = st.text_input("Napomena")
+
 uploaded_files = st.file_uploader(
     "Dodaj slike ili dokumente",
     type=["jpg", "jpeg", "png", "pdf"],
